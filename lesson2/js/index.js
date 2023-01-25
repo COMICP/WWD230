8b56ab373a1831be62add654770d223855ffce3e
Jaden Binette <binettejaden@gmail.com>
2023-01-25 16:14:11 -0500

    fix errors

---
+++ lesson2/js/index.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('LastUpdated').textContent = "Last Updated: " + document.lastModified;
+
+
+
+document.getElementById('copy').textContent = "©"+ new Date().getFullYear() +  " .:|:. Jaden M. Binette .:|:. Virginia";
