33ff98ee31cd7081389d38cdd0877f5aa6cc47f9
Jaden Binette <binettejaden@gmail.com>
2023-01-29 22:46:36 -0500

    header and footer

---
+++ chamber/scripts/index.js
@@ -0,0 +1,19 @@
+
+
+document.getElementById('LastUpdated').textContent = "Last Updated: " + document.lastModified;
+
+const date = new Date();
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+let lday = weekday[date.getDay()];
+
+let day = date.getDate();
+let month = date.toLocaleString('default', { month: 'long' });
+let year = date.getFullYear();
+
+let currentDate = `${lday}, ${day}-${month}-${year}`;
+
+document.getElementById('copy').textContent = "©"+ new Date().getFullYear() +  " Fredericksburg Chamber";
+document.getElementById('date').textContent =  currentDate;
