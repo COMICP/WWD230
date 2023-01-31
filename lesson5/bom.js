64614b57e88e2b5b5ffc80ceff38d87806dd3359
Jaden Binette <binettejaden@gmail.com>
2023-01-31 15:57:25 -0500

    List thing

---
+++ lesson5/bom.js
@@ -0,0 +1,32 @@
+
+const button = document.getElementById("submit");
+const list = document.getElementById("list");
+
+
+
+function send(){
+     let inp = document.getElementById("favchap").value;
+
+    if (inp !== ""){
+        let li = document.createElement("li");
+        let x = document.createElement("button");
+        li.textContent = inp;
+        x.textContent = "X";
+        li.append(x);
+        list.append(li);
+        document.getElementById("favchap").setAttribute('value', "");
+        document.getElementById("favchap").setAttribute('Placeholder', "");
+        document.getElementById("favchap").value = "";
+        function rem(){
+            li.remove()
+        }
+        x.addEventListener("click", rem);
+    }
+    
+}
+
+
+
+
+button.addEventListener("click", send)
+
