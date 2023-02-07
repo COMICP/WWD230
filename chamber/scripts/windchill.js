7c79f7af1145dbc556cba438811fc750965443d9
Jaden Binette <binettejaden@gmail.com>
2023-02-06 20:47:15 -0500

    weather and sizes

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,28 @@
+let tempc = 2;
+let tempf = (tempc * 1.8) + 32;
+let sk = 5;
+let sm = sk / 1.609344;
+let chill = 'error';
+
+
+if (tempf <= 50 ){
+    if (sm >= 3){
+        windchill();
+    }
+    else{
+        chill = "na";
+    }
+}
+else{
+    chill = "na";
+}
+function windchill(){
+    let factor = 35.74 + (0.6215*tempf) - (35.75*sm^0.16) + (0.4275*tempf*sm^0.16);
+    chill = tempf + Math.round(factor);
+
+
+}
+
+document.getElementById('chill').textContent =  chill + "°";
+document.getElementById('temp').textContent =  tempf + "°";
+document.getElementById('speed').textContent =  Math.round(sm) + "mph";
