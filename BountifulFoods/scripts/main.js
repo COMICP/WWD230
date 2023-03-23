fb31298536816da819cc3dfbb21f7518323b5c61
Jaden Binette <binettejaden@gmail.com>
2023-03-23 14:15:22 -0400

    Home Page

---
+++ BountifulFoods/scripts/main.js
@@ -0,0 +1,38 @@
+
+
+document.getElementById('LastUpdated').textContent = "Last Updated: " + document.lastModified;
+
+const date = new Date();
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+let lday = weekday[date.getDay()];
+
+let day = date.getDate();
+let month = date.toLocaleString('default', { month: 'long' });
+let year = date.getFullYear();
+
+
+let currentDate = `${lday}, ${day}-${month}-${year}`;
+
+document.getElementById('copy').textContent = "©"+ new Date().getFullYear() +  " Bountiful Foods";
+document.getElementById('date').textContent =  currentDate;
+
+function toggle(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerButton").classList.toggle("open");
+
+}
+const x = document.getElementById("hamburgerButton");
+x.onclick = toggle;
+
+if (lday == "Monday" || lday == "Tuesday"){
+    let header =document.querySelector("header");
+    let banor = document.createElement('h2');
+    banor.setAttribute("id", "banorADD");
+    banor.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    header.appendChild(banor);
+
+}
+
---
+++ BountifulFoods/scripts/windchill.js
@@ -0,0 +1,90 @@
+let tempc = 0;
+let tempf = (tempc * 1.8) + 32;
+let sk = 5;
+let sm = 0;
+let chill = 'error';
+
+
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weatherimg');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=los Angeles&appid=5ea88fe6f0f9f7a30b39c94f6212955e&units=imperial';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); 
+        displayResults(data);
+
+
+
+
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+
+
+function  displayResults(weatherData) {
+
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+
+    sm = parseFloat(weatherData.wind.speed) ;
+    let hum = weatherData.main.humidity
+    tempf = parseFloat(weatherData.main.temp.toFixed(0)) ;
+    const desc = weatherData.weather[0].description;
+    
+
+    if (tempf <= 50 ){
+        if (sm >= 3){
+            windchill();
+        }
+        else{
+            chill = "na";
+        }
+    }
+    else{
+        chill = "na";
+    }
+
+    document.getElementById('temp').textContent =  tempf + "°";
+    document.getElementById('curr').textContent = desc;
+    document.getElementById('hum').textContent = hum
+
+
+
+
+  }
+
+apiFetch();
+
+
+if (tempf <= 50 ){
+    if (sm >= 3){
+        windchill();
+    }
+    else{
+        chill = "na";
+    }
+}
+else{
+    chill = "na";
+}
+function windchill(){
+    let T = tempf;
+    let V = sm
+    let factor = 0.0817*(3.71*V**0.5 + 5.81 -0.25*V)*(T - 91.4) + 91.4 ;
+    chill = Math.round(factor);
+
+
+}
+
+
+
